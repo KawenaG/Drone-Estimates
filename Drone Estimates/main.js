c71b9e292ba455e713b629acffe82fb4e00ca81c
KawenaG <45134325+KawenaG@users.noreply.github.com>
2023-08-14 16:07:08 -0700

    Working Search Bar
    fixed search bar location and added auto fill, added css styling to form and put entire form in container

--- Drone Estimates/main.js
+++ Drone Estimates/main.js
@@ -2,44 +2,39 @@ let map;
 let drawingManager;
 
 function initMap() {
-    map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: 37.7749, lng: -122.4194 }, // You can change these coordinates
-        zoom: 14,
-    });
-
-    drawingManager = new google.maps.drawing.DrawingManager({
-        drawingMode: google.maps.drawing.OverlayType.POLYGON,
-        drawingControl: true,
-        drawingControlOptions: {
-            position: google.maps.ControlPosition.TOP_CENTER,
-            drawingModes: ['polygon'],
-        },
-        polygonOptions: {
-            fillOpacity: 0.5,
-            strokeWeight: 2,
-        },
-    });
-
-    drawingManager.setMap(map);
-
-    google.maps.event.addListener(drawingManager, 'overlaycomplete', function(e) {
-      if (e.type === 'polygon') {
-          let path = e.overlay.getPath();
-          let areaInMeters = google.maps.geometry.spherical.computeArea(path);
-          let areaInFeet = areaInMeters * 10.764; // Convert from square meters to square feet
-          document.getElementById('area').innerHTML = `Area: ${areaInFeet.toFixed(2)} sq feet`;
-      }
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 37.7749, lng: -122.4194}, // Default to San Francisco, adjust as needed
+    zoom: 10,
   });
-  
-}
 
-// Create a search box and link it to the address input
-const input = document.getElementById('addressInput');
-const searchBox = new google.maps.places.SearchBox(input);
-map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+  drawingManager = new google.maps.drawing.DrawingManager({
+    drawingMode: google.maps.drawing.OverlayType.POLYGON,
+    drawingControl: true,
+    drawingControlOptions: {
+      position: google.maps.ControlPosition.TOP_CENTER,
+      drawingModes: ['polygon']
+    },
+    polygonOptions: {
+      editable: true,
+      draggable: true
+    }
+  });
+  drawingManager.setMap(map);
 
-// Listen for the event when the user selects a prediction
-searchBox.addListener('places_changed', () => {
+  google.maps.event.addListener(drawingManager, 'overlaycomplete', function(e) {
+    if (e.type === 'polygon') {
+      let path = e.overlay.getPath();
+      let areaInMeters = google.maps.geometry.spherical.computeArea(path);
+      let areaInFeet = areaInMeters * 10.764;
+      document.getElementById('area').innerHTML = `Area: ${areaInFeet.toFixed(2)} sq feet`;
+    }
+  });
+
+  // Search Box
+  const input = document.getElementById('addressInput');
+  const searchBox = new google.maps.places.SearchBox(input);
+
+  document.getElementById('searchButton').addEventListener('click', function() {
     const places = searchBox.getPlaces();
     
     if (places.length === 0) return;
@@ -48,11 +43,11 @@ searchBox.addListener('places_changed', () => {
     
     if (!place.geometry) return;
 
-    // Center the map on the selected place
     if (place.geometry.viewport) {
         map.fitBounds(place.geometry.viewport);
     } else {
         map.setCenter(place.geometry.location);
         map.setZoom(17);
     }
-});
+  });
+}
